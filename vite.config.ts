72ef3328c1d0c4d399caf38fd6e25b73665cf637
한수지 <fatholic5@gmail.com>
2025-04-11 06:05:27 +0900

    chore: 배포 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.ts",
-      coverage: {
-        reportsDirectory: "./.coverage",
-        reporter: ["lcov", "json", "json-summary"],
+export default defineConfig(({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return mergeConfig(
+    defineConfig({
+      plugins: [react()],
+
+      base: env.VITE_BASE_URL ?? "/",
+    }),
+    defineTestConfig({
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.ts",
+        coverage: {
+          reportsDirectory: "./.coverage",
+          reporter: ["lcov", "json", "json-summary"],
+        },
       },
-    },
-  }),
-);
+    }),
+  );
+});
